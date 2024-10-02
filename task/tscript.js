4491f458b311ff9e3aba5be0cc16bab2c4634b88
Your Name <you@example.com>
2024-10-02 18:57:12 +0530

    added all files

---
+++ task/tscript.js
@@ -0,0 +1,77 @@
+//fot loop
+//1st
+for (var a=0;a<=10;a++){
+    console.log(a)
+}
+//2nd
+for (var a=4;a<=10;a++){
+    if(a%2==0){
+        console.log(a);
+        
+    }
+}
+//3rd
+for (var a=1;a<=15;a++){
+    if (a%2==1){
+        console.log(a);
+        
+    }
+}
+//4th
+var a=36;
+if (a>=35&&a<45){
+    console.log(a+ "  grade D");
+    
+}
+//while
+//1st
+var a=0;
+while(a<=10){
+    console.log(a);
+    a++
+    
+}
+//2nd
+var a=1;
+while (a <= 10){
+    if (a%2==0){
+    console.log(a)
+    }
+    a++;
+}
+//3rd
+var a = 1;
+while (a <= 10) {
+    if (a % 2 === 1) { // Check if 'a' is divisible by 2 (even number)
+        console.log(a);
+    }
+    a++;
+}
+//do file
+//1st
+var a=0;
+do {
+    console.log(a);
+    a++
+    
+}
+while(a<=10)
+//2nd
+var a=2;                                    //starting from 1st even num to get even nums         
+do{
+    
+    console.log(a);
+    a+=2
+
+}
+while(a<=10);
+//3rd
+var b=1;                                     //starting from 1st odd num to get odd nums
+do{
+    console.log(b);
+    b+=2
+    
+}
+while(b<=10)
+
+  
